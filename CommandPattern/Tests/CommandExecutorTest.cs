8e1824b3ba74f1115294a36821946e3a6c7cfe01
Connor Conway <connor_conway1@outlook.com>
2018-02-12 13:07:36 +0000

    Refactoring away the code smell of a giant switch statement using the Command Pattern

---
+++ CommandPattern/Tests/CommandExecutorTest.cs
@@ -0,0 +1,16 @@
+﻿using CommandPattern.Library;
+using NUnit.Framework;
+
+namespace CommandPattern.Tests
+{
+	[TestFixture]
+    public class CommandExecutorTest
+    {
+	    [Test]
+	    public void ToDo()
+	    {
+		    var executor = new CommandExecutor();
+			executor.ExecuteCommand(new [] { "UpdateQuantity" , "10"});
+	    }
+    }
+}
