44eca27df6a0a1573fa9acbc84879d239702d9da
Connor Conway <connor_conway1@outlook.com>
2018-02-25 19:25:11 +0000

    Start looking at the memento pattern as an alternative to the command pattern for undoing and redoing states

---
+++ MementoPattern/Model/SalesProspect.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MementoPattern.Model
+{
+	public class SalesProspect
+	{
+		public double Budget { get; set; }
+		public string Name { get; set; }
+		public string Phone { get; set; }
+
+		// Stores memento
+
+		public Memento SaveMemento()
+		{
+			Console.WriteLine("\nSaving state --\n");
+			return new Memento(Name, Phone, Budget);
+		}
+
+		// Restores memento
+
+		public void RestoreMemento(Memento memento)
+		{
+			Console.WriteLine("\nRestoring state --\n");
+			Name = memento.Name;
+			Phone = memento.Phone;
+			Budget = memento.Budget;
+		}
+	}
+
+	public class Memento
+	{
+		public string Name;
+		public string Phone;
+		public double Budget;
+
+		public Memento(string name, string phone, double budget)
+		{
+			this.Name = name;
+			this.Phone = phone;
+			this.Budget = budget;
+		}
+	}
+}
